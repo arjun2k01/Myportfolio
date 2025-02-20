cd265614890cf37215ebdac98b9a83ad407ed3b0
arjun2k01 <122463451+arjun2k01@users.noreply.github.com>
2025-02-20 12:26:31 +0530

    download cv added

--- src/components/ResumeDownload.jsx
+++ src/components/ResumeDownload.jsx
@@ -3,7 +3,7 @@ import { ButtonPrimary } from "./Button";
 const ResumeDownload = () => {
   return (
     <ButtonPrimary
-      href="https://drive.google.com/file/d/1EUDm3eww7YxY2ya7z6tf53uydcw8cuhr/view?usp=drive_link" // Ensure the resume is in the public folder
+      href="https://drive.google.com/uc?export=download&id=1EUDm3eww7YxY2ya7z6tf53uydcw8cuhr" // Ensure the resume is in the public folder
       label="Download CV"
       icon="download"
       classes="my-custom-class"
